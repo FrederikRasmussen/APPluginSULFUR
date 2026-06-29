72b10a8b4601c553b33ad2ca6f49c7a1631924a8
Frederik Rasmussen <lundbyft@gmail.com>
2026-06-29 22:06:31 +0200

    Lots of testing related to loot tables

--- Patches/DialogController/OnMultipleChoiceRequest.cs
+++ Patches/DialogController/OnMultipleChoiceRequest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 using Archipelago.Helpers;
 using Archipelago.MultiClient.Net;
 using Archipelago.MultiClient.Net.Enums;
@@ -6,6 +8,10 @@ using HarmonyLib;
 using I2.Loc;
 using NodeCanvas.DialogueTrees;
 using PerfectRandom.Sulfur.Core;
+using PerfectRandom.Sulfur.Core.Items;
+using PerfectRandom.Sulfur.Core.Stats;
+using Unity.Serialization.Json;
+using UnityEngine;
 
 namespace Archipelago.Patches;
 
@@ -16,9 +22,82 @@ public class OnMultipleChoiceRequest
     
     public static void Postfix(MultipleChoiceRequestInfo info, DialogController __instance)
     {
+        foreach (ItemId itemId in typeof(ItemIds).GetFields().Select(f => f.GetValue(null)))
+        {
+            var definition = itemId.GetAsset();
+            var builder = new StringBuilder();
+            builder.Append(definition.name);
+            builder.Append(", ");
+            builder.Append(definition.ItemType);
+            builder.Append(", ");
+            if (definition.ItemType is ItemType.Consumable)
+            {
+                foreach (var buff in definition.buffsOnConsume)
+                {
+                    if (buff.attributeId == EntityAttributes.Stat_HealthRegen)
+                    {
+                        builder.Append(buff.totalValueOverride);
+                        builder.Append(", ");
+                    }
+                }
+            }
+
+            builder.Append(definition.basePrice);
+            builder.Append(", ");
+            builder.Append(definition.itemQuality);
+            
+            Plugin.Logger.LogInfo(builder);
+        }
+        
+        /*var builder = new StringBuilder();
+        builder.Append("\n");
+        builder.Append("[\n");
+        var lootTables = Resources.FindObjectsOfTypeAll<LootTable>().OrderBy(lootTable => lootTable.name).ToArray();
+        foreach (var lootTable in lootTables)
+        {
+            builder.Append("  {\n");
+            builder.Append("    \"Name\": \"" + lootTable.name + "\",\n");
+            builder.Append("    \"Entries\":" + "\n");
+            builder.Append("    {\n");
+            foreach (var entry in lootTable.entries)
+            {
+                builder.Append("       \"" + $"{entry.lootItem}".Replace("\"", "\\\"") + "\": "+ entry.lootWeight);
+                builder.Append(entry.Equals(lootTable.entries[^1]) ? "\n" : ",\n");
+            }
+            builder.Append("    }\n");
+            builder.Append("  }");
+            builder.Append(lootTable.Equals(lootTables[^1]) ? "\n" : ",\n");
+        }
+        builder.Append("]");
+        Plugin.Logger.LogInfo(builder);
+        
+        builder = new StringBuilder();
+        builder.Append("\n");
+        builder.Append("[\n");
+        foreach (var lootTable in lootTables.OrderBy(lootTable => lootTable.name))
+        {
+            builder.Append("  {\n");
+            builder.Append("    \"Name\": \"" + lootTable.name + "\",\n");
+            builder.Append("    \"Entries\":" + "\n");
+            builder.Append("    {\n");
+            var entries = lootTable.DebugGetLootChancePerObject().OrderByDescending(entry => entry.Value).ToList();
+            foreach (var entry in entries)
+            {
+                builder.Append("       \"" + $"{entry.Key}".Replace("\"", "\\\"") + "\": "+ entry.Value);
+                builder.Append(entry.Equals(lootTable.entries[^1]) ? "\n" : ",\n");
+            }
+            builder.Append("    }\n");
+            builder.Append("  }");
+            builder.Append(lootTable.Equals(lootTables[^1]) ? "\n" : ",\n");
+        }
+        builder.Append("]");
+        Plugin.Logger.LogInfo(builder);
+        */
+        
         if (Plugin.Client.Connected()) return;
         LocalizationManager.TryGetTranslation("UnitNames/Amulet", out var translatedAmuletName);
         if (__instance.CurrentSpeakable.ActorName != translatedAmuletName) return;
+        
         foreach (var button in __instance.playerDialogButtons)
         {
             var option = button.GetComponent<DialogOption>();
---
+++ Patches/LootTable/SelectOneItem.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using PerfectRandom.Sulfur.Core;
+using PerfectRandom.Sulfur.Core.Items;
+using PerfectRandom.Sulfur.Core.Stats;
+using PerfectRandom.Sulfur.Core.Units;
+using UnityEngine;
+
+namespace Archipelago.Patches;
+
+//[HarmonyPatch(typeof(LootManager), nameof(LootManager.SelectItemsFromTable))]
+public class SelectOneItem
+{
+    public static void Postfix(LootTable lootTable, List<ItemDefinition> __result, LootManager __instance)
+    {
+        if (lootTable is null)
+        {
+            Plugin.Logger.LogInfo("No table");
+            return;
+        }
+
+        if (__result == null || __result.Count == 0)
+            Plugin.Logger.LogInfo("Result was empty");
+        Plugin.Logger.LogInfo("Selecting from table:" + lootTable.name);
+        Plugin.Logger.LogInfo("With items: " + string.Join(", ",
+            lootTable.entries.Select(entry => $"{entry.lootItem}: {entry.lootWeight}")));
+        Plugin.Logger.LogInfo("Selected: " + string.Join(", ", __result.Select(item => item.name)));
+    }
+}
+
+//[HarmonyPatch(typeof(Unit), nameof(Unit.SpawnLoot))]
+public class UnitStuff
+{
+    public static void Prefix(Unit __instance)
+    {
+        GlobalSettings.VerboseLoggingLoot = true;
+        Plugin.Logger.LogInfo("Checking to spawn?" +
+                              $"{__instance.IsCivilian}, {__instance.unitSO.shouldDropLoot}, {__instance.hasDroppedLoot}");
+        if (!__instance.IsCivilian && __instance.unitSO.shouldDropLoot && !__instance.hasDroppedLoot)
+        {
+            Plugin.Logger.LogInfo(__instance + " trying to spawn loot");
+        }
+    }
+}
+
+[HarmonyPatch(typeof(LootManager), nameof(LootManager.GetLootTypeDecision))]
+public class TypeDecision
+{
+    public static void Prefix(LootManager __instance)
+    {
+        LootSettings settings = GameManager.Instance.Settings.LootSettings; // __instance.lootSettings;
+        Plugin.Logger.LogInfo("Found settings " + settings);
+        var weights = new List<int>
+        {
+            settings.weightAmmo,
+            settings.weightAttachments,
+            settings.weightEnchantments,
+            settings.weightEnchantmentsOil,
+            settings.weightEquipment,
+            settings.weightGadgets,
+            settings.weightQuestItems,
+            settings.weightScavenge,
+            settings.weightValuables,
+            settings.weightWeapons
+        };
+        settings.weightWeapons = 10000;
+        Plugin.Logger.LogInfo("Weights: " + string.Join(", ", weights));
+    }
+}
